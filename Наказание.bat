47f3a6160fb4a4f6ec2b0c39533035930da7a8a8
luntik4343 <69987863+luntik4343@users.noreply.github.com>
2020-08-20 22:28:21 +0300

    лол
    лол

---
+++ Наказание.bat
@@ -0,0 +1,61 @@
+@echo off
+CLS
+:A
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+start iexplore.exe
+echo Ошибка iexplore.exe. БЕГИТЕ ГЛУПЦЫ.
+echo Сдавайся
+echo Ошибка word.exe и notepad.exe. Ошибка.
+start winword.exe
+start notepad.exe 
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+echo Ты повелся? ГЫ. Создано Лунтеком
+pause
+start calc.exe
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+echo БЕГИ ГЛУПЕЦ!
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+start notepad.exe
+pause
